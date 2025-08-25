c8fefbf5de7e91b25844c686212d2a67d74b2467
Adrian Murillo <adrianmurilloaraya@gmail.com>
2025-08-25 15:30:00 -0600

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,19 +16,8 @@ variable "env" {
   default     = "dev"
 }
 
-variable "cluster_name" {
-  type = string
-}
 
 variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidrs" {
-  type = list(string)
-}
-
-variable "azs" {
-  type = list(string)
 }
