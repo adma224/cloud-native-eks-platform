3a6b85b46f286ff18656754440a4764aa375fce3
Adrian Murillo <adrianmurilloaraya@gmail.com>
2025-08-25 16:10:55 -0600

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3"{
         bucket = "org-adrian-aws-eks-app-platform-tfstate-924917171175"
-        key = "terraform.tfstate"
+        key = "dev/terraform.tfstate"
         region = "us-east-1"
         dynamodb_table = "org-adrian-aws-eks-app-platform-tflock"
         encrypt = true
