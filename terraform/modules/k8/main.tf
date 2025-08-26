595425c5aece4e3f5b0f895b1a8ecf1caf27cf21
Adrian Murillo <adrianmurilloaraya@gmail.com>
2025-08-25 21:13:52 -0600

    Update main.tf

--- terraform/modules/k8/main.tf
+++ terraform/modules/k8/main.tf
@@ -1,4 +1,7 @@
-# IAM role that EKS control plane assumes
+##############################
+# EKS control plane (minimal)
+##############################
+
 resource "aws_iam_role" "cluster" {
   name = "${var.cluster_name}-cluster-role"
   assume_role_policy = jsonencode({
@@ -11,39 +14,29 @@ resource "aws_iam_role" "cluster" {
   })
 }
 
-# Minimal required permissions for the control plane
 resource "aws_iam_role_policy_attachment" "cluster_policy" {
   role       = aws_iam_role.cluster.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "vpc_resource_controller" {
-  role       = aws_iam_role.cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_VPCResourceController"
-}
-
-# The cluster itself (no nodegroups yet)
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   version  = var.kubernetes_version
   role_arn = aws_iam_role.cluster.arn
 
   vpc_config {
-    # Use your PRIVATE subnets; control plane ENIs go here.
-    subnet_ids = var.private_subnet_ids
-    endpoint_public_access  = true   # simplest for MVP; can tighten later
+    subnet_ids              = var.private_subnet_ids
+    endpoint_public_access  = true
     endpoint_private_access = false
   }
 
-  # Tagging comes from provider.default_tags automatically
+  depends_on = [aws_iam_role_policy_attachment.cluster_policy]
 }
 
-
 ########################################
-# Managed node group (EC2, private subnets)
+# Managed node group (EC2, private)
 ########################################
 
-# IAM role for the nodes
 resource "aws_iam_role" "node" {
   name = "${var.cluster_name}-node-role"
   assume_role_policy = jsonencode({
@@ -56,7 +49,6 @@ resource "aws_iam_role" "node" {
   })
 }
 
-# Required managed policies for nodes
 resource "aws_iam_role_policy_attachment" "node_worker" {
   role       = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
@@ -72,7 +64,6 @@ resource "aws_iam_role_policy_attachment" "node_ecr" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-# Managed node group in PRIVATE subnets
 resource "aws_eks_node_group" "default" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-ng"
@@ -87,14 +78,12 @@ resource "aws_eks_node_group" "default" {
 
   instance_types = [var.node_instance_type]
   capacity_type  = "ON_DEMAND"
-  ami_type       = "AL2_x86_64" # good default for t3.* nodes
+  ami_type       = "AL2_x86_64"
 
-  # Ensure policies are attached before NG creation; and cluster IAM is ready
   depends_on = [
-    aws_iam_role_policy_attachment.cluster_policy,
-    aws_iam_role_policy_attachment.vpc_resource_controller,
     aws_iam_role_policy_attachment.node_worker,
     aws_iam_role_policy_attachment.node_cni,
-    aws_iam_role_policy_attachment.node_ecr
+    aws_iam_role_policy_attachment.node_ecr,
+    aws_eks_cluster.this
   ]
 }
