49e6a31e68f1d2f35185a5e18503671e1ce66ebe
Adrian Murillo <adrianmurilloaraya@gmail.com>
2025-08-26 14:47:57 -0600

    Fixed providers issue

--- terraform/k8-smoke.tf
+++ terraform/k8-smoke.tf
@@ -1,10 +1,7 @@
 resource "kubernetes_namespace" "obs_system" {
   metadata {
     name = "obs-system"
-    labels = {
-        project = var.project
-        env = var.env
-    }
+    labels = { project = var.project, env = var.env }
   }
-  depends_on = [ module.k8 ]
+  depends_on = [module.k8]  # ensure cluster exists before talking to it
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,19 +12,19 @@ provider "aws" {
 provider "tls" {}
 
 data "aws_eks_cluster" "this" {
-  name = module.k8.cluster_name
-  depends_on = [ module.k8 ]
+  name       = module.k8.cluster_name
+  depends_on = [module.k8]
 }
 
-
+# Get a short-lived auth token for that cluster
 data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-  depends_on = [ module.k8 ]
+  name       = module.k8.cluster_name
+  depends_on = [module.k8]
 }
 
-
+# Kubernetes provider wired to your EKS control plane
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
