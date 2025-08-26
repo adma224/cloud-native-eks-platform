ce9b2b28298e77ede0e78492027b16e10b58c976
Adrian Murillo <adrianmurilloaraya@gmail.com>
2025-08-26 13:33:32 -0600

    Added cluster connection details to output

--- terraform/modules/k8/outputs.tf
+++ terraform/modules/k8/outputs.tf
@@ -4,3 +4,6 @@ output "cluster_version" { value = aws_eks_cluster.this.version }
 output "cluster_endpoint"{ value = aws_eks_cluster.this.endpoint }
 output "nodegroup_name" { value = aws_eks_node_group.default.node_group_name }
 output "node_role_arn"  { value = aws_iam_role.node.arn }
+output "cluster_ca_data" { value = aws_eks_cluster.this.certificate_authority[0].data }
+output "oidc_issuer_url" { value = aws_eks_cluster.this.identity[0].oidc[0].issuer }
+
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,50 @@ output "region"     { value = data.aws_region.current.name }
 output "vpc_id"             { value = module.vpc.vpc_id }
 output "public_subnet_ids"  { value = module.vpc.public_subnet_ids }
 output "private_subnet_ids" { value = module.vpc.private_subnet_ids }
+
+output "cluster_name" {
+  value = module.k8.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.k8.cluster_endpoint
+}
+
+output "cluster_oidc_url" {
+  value = module.k8.oidc_issuer_url
+}
+
+
+# Handy: drop-in kubeconfig that uses AWS CLI token exec
+# Save it with: terraform -chdir=terraform output -raw kubeconfig_yaml > kubeconfig.yaml
+output "kubeconfig_yaml" {
+  value = <<-EOT
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${module.k8.cluster_endpoint}
+    certificate-authority-data: ${module.k8.cluster_ca_data}
+  name: ${module.k8.cluster_name}
+contexts:
+- context:
+    cluster: ${module.k8.cluster_name}
+    user: ${module.k8.cluster_name}
+  name: ${module.k8.cluster_name}
+current-context: ${module.k8.cluster_name}
+kind: Config
+preferences: {}
+users:
+- name: ${module.k8.cluster_name}
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1beta1
+      command: aws
+      args:
+        - eks
+        - get-token
+        - --region
+        - ${data.aws_region.current.name}
+        - --cluster-name
+        - ${module.k8.cluster_name}
+  EOT
+}
